d38ca76435d87450333802be5820c7f750771711
Owais <owaisstar@gmail.com>
2021-12-29 08:59:00 +0500

    validations updated

--- routes/payment.js
+++ routes/payment.js
@@ -8,11 +8,11 @@ const { body, validationResult } = require('express-validator');
 router.post('/',
 
 /* Sanitization and validations */
-body('cardNumber').isLength({min:16}).withMessage("card number should be of 16 digits"),
+body('cardNumber').isLength({min:16}).isNumeric().withMessage("card number should be of 16 digits numeric"),
 
-body('cvv').isLength({min:3}),
+body('cvv').isLength({min:3}).isNumeric(),
 
-body('expiry').isLength({min:5}),
+body('expiry').isLength({min:5}).matches("/^(0[1-9]|1[0-2])\/?([0-9]{4}|[0-9]{2})$/mg"),
 
 body('name').trim().escape(),
 
@@ -23,7 +23,7 @@ body('name').trim().escape(),
         return res.status(400).json({status:0,message:errors.array(),data:{}})
     }
     /* call respective controller function on passing all the validations */
-    paymentCOntroller.create(req,res)
+    // paymentCOntroller.create(req,res)
 });
 
 module.exports = router;
